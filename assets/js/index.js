c3dcab6d5c072266c3504a8b09d677ce1f61a83e
khalilalmasri <96783293+khalilalmasri@users.noreply.github.com>
2024-11-08 18:07:04 +0200

    123

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,16 +6,18 @@ function testbtn() {
   console.log(switchmenu);
   console.log(menuIcon.attributes.src.value);
   if (switchmenu) {
-    menubtn.style = "display: flex ; overflow: hidden;";
+    menubtn.style = "display: flex ; overflow: hidden; ";
     menubtn.classList.add("active_menu");
-    menuIcon.classList.add("active_arrow");
+    // menuIcon.classList.add("active_arrow");
     menuIcon.classList.remove("active_menuarrow");
-    menuIcon.attributes.src.value = "assets/icons/back.svg";
+    // menuIcon.attributes.src.value = "assets/icons/exit.svg";
+    menuIcon.style = "opacity: 0.2;";
   } else {
     menubtn.style = "display: none; ";
     menuIcon.classList.remove("active_arrow");
     menuIcon.classList.add("active_menuarrow");
-    menuIcon.attributes.src.value = "assets/icons/menu.svg";
+    // menuIcon.attributes.src.value = "assets/icons/menu.svg";
+    menuIcon.style = "width: 33px; height: 25px;";
   }
 }
 // menuIcon.addEventListener("click", testbtn)
